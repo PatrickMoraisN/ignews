0a5b1d15cd42f7c217b4805f6926c266bf18823d
Patrick Morais <ppternunes@gmail.com>
2021-09-07 18:30:39 -0300

    global style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,7 @@
-import styles from '../styles/home.module.scss';
 import Head from 'next/head';
 export default function Home() {
   return (
-    <h1 className={styles.title}>
+    <h1>
       Hello World!
       <Head>
         <title>Home Ignews</title>
