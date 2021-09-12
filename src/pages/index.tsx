e28cb97b04d7885eed88fd46a325e97a852d0e0a
Patrick Morais <ppternunes@gmail.com>
2021-09-12 13:46:38 -0300

    home styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,13 @@
 import Head from 'next/head';
+import styles from './home.module.scss'
 export default function Home() {
   return (
     <h1>
       <Head>
         <title>Home Ignews</title>
       </Head>
-      <main>
-        <section>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
           <span>Hey, welcome</span>
           <h1>News about the <span>React</span> world.</h1>
           <p>Get acces to all publications <br />
