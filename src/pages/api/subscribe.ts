4d6be2bf7c04c87c3aa87959eb6f4b8bcedd34ef
Patrick Morais <ppternunes@gmail.com>
2021-09-19 18:11:14 -0300

    stripe customer / getSession / create session stripe

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,10 +1,19 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { getSession } from "next-auth/client";
 import { stripe } from "../../services/stripe";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   // Requisicao para criar coisas
   if(req.method === 'POST') {
-    const checkoutSession = await stripe.checkout.sessions.create({
+    const session = await getSession({ req });
+
+    const stripeCustomer = await stripe.customers.create({
+      email: session.user.email,
+    })
+
+    const stripeCheckoutSession = await stripe.checkout.sessions.create({
+      // id do customer no stripe, e nao no banco de dados Fauna
+      customer: stripeCustomer.id,
       payment_method_types: ['card'],
       billing_address_collection: 'required',
       line_items: [
@@ -15,10 +24,11 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
       ],
       mode: 'subscription',
       allow_promotion_codes: true,
-      success_url: 'http://localhost:3000/posts',
-      cancel_url:  'http://localhost:3000/'
+      success_url: process.env.STRIPE_SUCCESS_URL,
+      cancel_url:  process.env.STRIPE_CANCEL_URL
     })
 
+    return res.status(200).json({ sessionId: stripeCheckoutSession.id })
   } else {
     res.setHeader('Allow', 'POST');
     res.status(405).end('Method not allowed!');
