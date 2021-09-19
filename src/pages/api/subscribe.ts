72d4d274f8035430b86626971b491fe66e4ec55f
Patrick Morais <ppternunes@gmail.com>
2021-09-19 18:02:51 -0300

    checkout session func

---
+++ src/pages/api/subscribe.ts
@@ -0,0 +1,26 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { stripe } from "../../services/stripe";
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  // Requisicao para criar coisas
+  if(req.method === 'POST') {
+    const checkoutSession = await stripe.checkout.sessions.create({
+      payment_method_types: ['card'],
+      billing_address_collection: 'required',
+      line_items: [
+        {
+          price: 'price_1JYyQoCLxGFJTV3BvkUeAIjq',
+          quantity: 1,
+        }
+      ],
+      mode: 'subscription',
+      allow_promotion_codes: true,
+      success_url: 'http://localhost:3000/posts',
+      cancel_url:  'http://localhost:3000/'
+    })
+
+  } else {
+    res.setHeader('Allow', 'POST');
+    res.status(405).end('Method not allowed!');
+  }
+} 
