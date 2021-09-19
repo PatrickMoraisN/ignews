2c500f681b50b663874f14eb2253ba4ed2dc6656
Patrick Morais <ppternunes@gmail.com>
2021-09-19 18:28:48 -0300

    subscribe button getStripeJs / redirectToCheckout

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,5 +1,6 @@
 import { signIn, useSession } from 'next-auth/client'
 import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss'
 
 interface SubscribeButtonProps {
@@ -20,7 +21,11 @@ export function SubscribeButton({ priceId }: SubscribeButtonProps) {
 
       const { sessionId } = response.data;
 
-      
+      const stripe = await getStripeJs();
+
+      await stripe.redirectToCheckout(sessionId);
+    } catch (error) {
+      alert(error.message);
     }
   }
 
