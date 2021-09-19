1e09d693224dfcc397077f6f54f1a91f6ee410dd
Patrick Morais <ppternunes@gmail.com>
2021-09-19 18:19:48 -0300

    services api / starting axios post

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,20 +1,27 @@
 import { signIn, useSession } from 'next-auth/client'
+import { api } from '../../services/api';
 import styles from './styles.module.scss'
 
 interface SubscribeButtonProps {
   priceId: string;
 }
 
-export function SubscribeButton({ priceId }) {
+export function SubscribeButton({ priceId }: SubscribeButtonProps) {
   const [session] = useSession();
 
-  function handleSubscribe() {
+  async function handleSubscribe() {
     if(!session) {
       signIn('github');
       return;
     }
 
-    
+    try {
+      const response = await api.post('/subscribe');
+
+      const { sessionId } = response.data;
+
+      
+    }
   }
 
   return (
