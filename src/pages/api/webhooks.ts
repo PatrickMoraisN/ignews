5560382404d5b35e01eeb59d19fdeb4b2537d619
Patrick Morais <ppternunes@gmail.com>
2021-10-02 17:25:53 -0300

    add webhooks events

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -1,6 +1,55 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { Readable } from 'stream';
+import Stripe from "stripe";
+import { stripe } from "../../services/stripe";
 
-export default (req: NextApiRequest, res: NextApiResponse) => {
-  console.log('evento recebido');
-  res.status(200).json({ok: true});
+async function buffer(readable:Readable) {
+  const chunks = [];
+
+  for await (const chunk of readable) {
+    chunks.push(
+      typeof chunk === 'string' ? Buffer.from(chunk) : chunk
+    );
+  }
+  return Buffer.concat(chunks)
+}
+
+export const config = {
+  // Por padrao o Next entende que a requisicao vem como um JSON, mas nesse caso vem como um Readable(stream)
+  // Entao temos que desabilitar o `entendimento padrao` do Next.
+  api: {
+    bodyParser: false
+  }
+};
+
+const relevantEvents = new Set([
+  'checkout.session.completed'
+])
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  if(req.method === 'POST') {
+    const buff = await buffer(req)
+    // Chave que vem por requisicao do stripe
+    const secret = req.headers['stripe-signature'];
+
+    let event: Stripe.Event;
+
+    try {
+      event = stripe.webhooks.constructEvent(buff, secret, process.env.STRIPE_WEBHOOK)
+    } catch (err) {
+      return res.status(400).send("Webhook error " + err.message)
+    }
+
+    // Checar o tipo de eventos, sessao completada...
+    const { type } = event;
+
+    if (relevantEvents.has(type)) {
+      console.log('evento recebido ' + event)
+    }
+
+    res.json({ received: true});
+  } else {
+    res.setHeader('Allow', 'POST');
+    res.status(405).end('Method not allowed!');
+  }
 }
