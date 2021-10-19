6d665d2660d87d268627dbf8c8998b690b3e8fb3
Patrick Morais <ppternunes@gmail.com>
2021-10-19 12:58:26 -0300

    add slug style

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -4,6 +4,8 @@ import Head from "next/head";
 import { RichText } from "prismic-dom";
 import getPrismicClient from "../../services/prismic";
 
+import styles from './post.module.scss';
+
 interface PostProps {
   post: {
     slug: string;
@@ -20,11 +22,12 @@ export default function Post({ post }: PostProps) {
         <title>{post.title} | Ignews</title>
       </Head>
 
-      <main>
-        <article>
+      <main className={styles.container}>
+        <article className={styles.post}>
           <h1>{post.title}</h1>
           <time>{post.updatedAt}</time>
           <div
+            className={styles.postContent}
             dangerouslySetInnerHTML={{ __html: post.content }}
           />
         </article>
